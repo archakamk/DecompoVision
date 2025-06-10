c0e433a12b403128c1214d41e2cae77daf2c43d9
Kalyan <archakamk@gmail.com>
2025-06-09 18:11:58 -0700

    main added

---
+++ main.py
@@ -0,0 +1,18 @@
+from src.inference import run_inference
+import cv2
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    x = []
+    image_path = "indoor_room.jpg"
+    img, outputs = run_inference(image_path)
+    titles = ["Input", "Albedo", "Shading", "Shadow", "Specular"]
+    plt.figure(figsize=(15, 3))
+
+    for i, (title, out) in enumerate(zip(titles, [img] + outputs)):
+        plt.subplot(1, 5, i + 1)
+        plt.imshow(out)
+        plt.title(title)
+        plt.axis("off")
+        plt.tight_layout()
+        plt.show()
