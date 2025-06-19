354ceafdf29e6c7550659d51ef1ea9ef16f6d62b
Kalyan <archakamk@gmail.com>
2025-06-18 23:54:20 -0700

    updated composite loss function

--- inference.py
+++ inference.py
@@ -48,7 +48,7 @@ def visualize_image(input_tensor, albedo, shading, shadow, specular):
     plt.show()
 
 if __name__ == "__main__":
-    img_path = "data/sunset.jpg"
+    img_path = "data/indoor_room.jpg"
     input_tensor, albedo, shading, shadow, specular = run_inference(img_path)
 
     # Save outputs if needed
