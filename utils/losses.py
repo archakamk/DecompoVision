0640bbba93447f07e5d5dc6ea2fb7716b525da14
Kalyan <archakamk@gmail.com>
2025-06-19 01:06:54 -0700

    rearranged composite loss file

--- utils/losses.py
+++ utils/losses.py
@@ -1,19 +1,21 @@
 from pytorch_msssim import ssim
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
 class CompositeLoss(nn.Module):
-    def __init__(self, alpha=0.84):
+    def __init__(self):
         super(CompositeLoss, self).__init__()
-        
-        self.alpha = alpha
         self.mse = nn.MSELoss()
-    
+        self.alpha = 0.84
+
     def forward(self, pred, target):
         pred = torch.clamp(pred, 0, 1)
         target = torch.clamp(target, 0, 1)
 
         mse_loss = self.mse(pred, target)
         ssim_loss = 1 - ssim(pred, target, data_range=1.0, size_average=True)
-        loss = (1 - self.alpha) * mse_loss + self.alpha * ssim_loss
-        return loss
+
+        total_loss = (1 - self.alpha) * mse_loss + self.alpha * ssim_loss
+
+        return total_loss
