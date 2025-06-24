d33985dd6f9cdd65b2ed68146189823b0e8dd43c
Kalyan <archakamk@gmail.com>
2025-06-24 00:10:20 -0700

    figuring out training and dataset control

--- train.py
+++ train.py
@@ -1,4 +1,10 @@
 import torch
-import torch.nn as nn
+import torch.nn
 from utils.losses import CompositeLoss
 
+class Train(nn.module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
